cd9f43113dc109851de8da81ac3e8d4180ec01df
OCETP <pk@OCETP.local>
2022-05-05 19:13:49 +0300

    homework

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const container = document.getElementById('app');
 
 
 const renderPost = function(post) {
-    let title = document.createElement('h3');
+    let title = document.createElement('h3'); //создаем объект, ниже вытаскивая инфу из массива post
     title.innerText = post.title;
+    let button = document.createElement("button");
+    button.classList.add('cross');
+    button.innerHTML = "X";
     let img = document.createElement('img');
     img.src = post.cover;
     let description = document.createElement('p');
     description.innerText = post.description;
+    let spotify = document.createElement('a') // создаём ссылку
+    spotify.href = post.links.spotify;        // присваиваем значение ссылки из массива
+    spotify.innerText = 'Spotify';            // видимый текст ссылки
+    spotify.target = "_blank";                // переход на новую вкладку
+    let apple = document.createElement('a')
+    apple.href = post.links.apple;
+    apple.innerText = 'Apple Music'; 
+    apple.target = "_blank";
+
 
     const postWrapper = document.createElement('div');
     postWrapper.classList.add('postWrapper');
@@ -22,8 +34,12 @@ const renderPost = function(post) {
     textWrapper.classList.add('textWrapper');
 
     textWrapper.appendChild(title);
+    postWrapper.appendChild(button);
     textWrapper.appendChild(description);
+    textWrapper.appendChild(spotify);
+    textWrapper.appendChild(apple);
     postWrapper.appendChild(img);
+
     postWrapper.appendChild(textWrapper);
 
     container.appendChild(postWrapper);
--- initialData.js
+++ initialData.js
@@ -6,16 +6,16 @@ const data = [
       Последние дни, выходя из метро, я включаю первый трек с ep Ogre You Asshole - Ukareteiru Hito.
       Не знаю о чем поют японцы, но я им верю, все будет хорошо.
       Первый трек помогает проснуться и заряжает тебя на весь день, а остальные плавно вводят в повседневную рутину.`,
-      cover: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+      cover: 'https://m.media-amazon.com/images/I/51Y4BSn6UzL.jpg',
       links: {
-        spotify: '',
-        apple: ''
+        spotify: 'https://open.spotify.com/album/18q11WTHKtFYikCfIOBro0?si=daZZKWpsSkCTaBrgqDHSgA',
+        apple: 'https://music.apple.com/ru/album/%E6%B5%AE%E3%81%8B%E3%82%8C%E3%81%A6%E3%81%84%E3%82%8B%E4%BA%BA-ep/469847981'
       },
       highlighted: true,
     },
     {
       id: 2,
-      title: 'Ravedeath, 1972',
+      title: 'Tim Hecker - Ravedeath, 1972',
       description: `Ещё очень давно я полюбил нуарную музыку (о которой расскажу скоро в своём первом видео), дарк-джаз и эмбиент.
   
       Ravedeath, 1972 - шестой полноформатник канадского электронщика Тима Хекера.
@@ -25,10 +25,10 @@ const data = [
   
       Кстати, всегда считал, что различная эмбиент музыка является отличным саундтреком к жизни. Она плавно пульсирует в ритме нашего сердца, и все внешние звуки мира лишь дополняют её, или она их, но не мешают друг другу. Она делает тебя одиноким от общества, но соединяет с единым организмом природы и всего, что создано людьми.
       `,
-      cover: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+      cover: 'https://i.discogs.com/u_sMg6x0q_rAeeRobsHsWL-U3iB7xu9Sn3cV1ddPORU/rs:fit/g:sm/q:90/h:594/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI3NDQ3/NzctMTMwNDE5OTUz/NC5qcGVn.jpeg',
       links: {
-        spotify: '',
-        apple: ''
+        spotify: 'https://open.spotify.com/album/6Iu8toVsvCc3I4INxYiVIy?si=GpZFkV94TDeh3iXPkoNueA',
+        apple: 'https://music.apple.com/ru/album/ravedeath-1972/411842895'
       },
       highlighted: false,
     },
@@ -38,10 +38,10 @@ const data = [
       description: `Вчера вышел третий сингл группы VR SEX - Live In A Dream с дебютного полноформатника Rough Dimension. Грязные гитары, синты, низкий вокал. Похоже на хиты 80х, только ещё круче.
       Ещё советую послушать их релиз Cyber Crimes.
       `,
-      cover: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+      cover: 'https://images.genius.com/185541632f49a36b9652448c1caf660a.1000x1000x1.png',
       links: {
-        spotify: '',
-        apple: ''
+        spotify: 'https://open.spotify.com/album/3r6VjTrGLzOuNlH7AwdDNu?si=Thc5Wr_7Tmag6Xb0_cipUQ',
+        apple: 'https://music.apple.com/ru/album/rough-dimension/1597946775'
       },
       highlighted: true,
     }
